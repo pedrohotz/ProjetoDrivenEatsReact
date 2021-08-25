f82b252a55ba88481fd7872195e76d8453849e94
Pedro Hotz <pedrohotz2002@gmail.com>
2021-08-25 10:05:33 -0300

    Primeiro Commit, componentização iniciada

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Top from "./Top";
+import Content from "./Content";
+
+
+export default function App(){
+    return(
+        <div>
+            <Top />
+            <Content /> 
+        </div>
+    );
+}
---
+++ src/components/Top.js
@@ -0,0 +1,11 @@
+
+export default function Top(){
+    return(
+        <div class="topo">
+        <div class="content-topo">
+            <p class="titulo">DrivenEats</p>
+            <p class="sub">Sua comida em 6 minutos</p>
+        </div>     
+    </div>
+    );
+}
